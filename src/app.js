47ea714595ba5783c7ee6ce49c78009ccf9a9735
Shyam <gshyamprasad2001@gmail.com>
2025-03-07 14:31:07 +0530

    feat:dtatbase connection

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 const express = require("express");
-
+const User = require("./models/user");
 const app = express();
- const connectDB = require('./config/database')
+const connectDB = require("./config/database");
 
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Dhoni",
+    lastName: "Prasad",
+    emailID: "Kohli2001@gmail.com",
+    passWord: "Kohli1234",
+  };
 
- connectDB()
-   .then(() => {
-     console.log("Database connected sucessfully");
-     app.listen(3000, () => {
-       console.log("Server Listening on 3000");
-     });
+  try {
+    const user = new User(userObj);
+    await user.save();
 
-   })
-   .catch((err) => {
-     console.log("Failed to connect Database");
-   });
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (err) {
+    res.status(401).json({ message: "Error creating user" });
+  }
+});
 
+connectDB()
+  .then(() => {
+    console.log("Database connected sucessfully");
+    app.listen(3000, () => {
+      console.log("Server Listening on 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect Database");
+  });
